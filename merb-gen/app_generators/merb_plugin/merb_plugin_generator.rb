0ddd6517da8b8c1a2d081bac94803b3772516b09
Daniel Neighman <has.sox@gmail.com>
2008-02-07 23:36:55 +1100

    [merb-gen] Fixes the plugin generator

--- merb-gen/app_generators/merb_plugin/merb_plugin_generator.rb
+++ merb-gen/app_generators/merb_plugin/merb_plugin_generator.rb
@@ -5,7 +5,9 @@ class MerbPluginGenerator < Merb::GeneratorBase
   
   def initialize(args, runtime_options = {})
     @base = File.dirname(__FILE__)
+    @name = args.first
     super
+    @destination_root = @name    
   end
   
   protected
