a948fc9aeb8bf2d70e0910cfb8a5bf0e821a3570
Yehuda Katz <wycats@yehuda-katzs-macbook-pro.local>
2008-02-06 17:36:49 -0800

    Whoops.

--- merb-action-args/lib/merb-action-args/abstract_controller.rb
+++ merb-action-args/lib/merb-action-args/abstract_controller.rb
@@ -13,7 +13,6 @@ class Merb::AbstractController
   end
   
   def _call_action(action)
-    # [[:id], [:foo, 7]]
     args = self.class.action_argument_list[action].map do |arg_default|
       arg = arg_default[0]
       raise BadRequest unless params.key?(arg.to_sym) || (arg_default.size == 2)
