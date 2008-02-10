ca9772e1808afb83ce79e3c5e24934a3bf6dc282
Daniel Neighman <has.sox@gmail.com>
2008-02-11 02:03:27 +1100

    [merb-gen] Fixes spec_helper for apps so that it works

--- merb-gen/app_generators/merb/templates/spec/spec_helper.rb
+++ merb-gen/app_generators/merb/templates/spec/spec_helper.rb
@@ -1,10 +1,13 @@
-Merb.environment="test"
 $TESTING=true
+require 'rubygems'
+require 'merb-core'
+require 'merb-core/test/fake_request'
+require 'merb-core/test/request_helper'
 
 # TODO: Boot Merb, via the Test Rack adapter
-
-require 'merb/test/fake_request'
-require 'merb/test/request_helper'
+Merb.start :environment => (ENV['MERB_ENV'] || 'test'),
+           :adapter     => 'runner',
+           :merb_root  => File.join(File.dirname(__FILE__), ".." )
 
 Spec::Runner.configure do |config|
   config.include(Merb::Test::RequestHelper)
