cb5d34d7a836f5156f4cf82c013f568eaed88491
Ben Burkert <ben@benburkert.com>
2008-02-13 14:42:45 -0600

    added Merb::Test::Multipart helpers in ControllerHelper

--- merb-test/lib/helpers/controller_helper.rb
+++ merb-test/lib/helpers/controller_helper.rb
@@ -1,8 +1,7 @@
 module Merb
   module Test
     module ControllerHelper
-      #Not sure where ControllerExceptions moved to in 0.9.0+
-      #include Merb::ControllerExceptions
+      include Multipart
       include RequestHelper
     end
   end
