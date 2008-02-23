2f47168183275e892eba6e113c4f9bfb2ff04746
Ben Burkert <ben@benburkert.com>
2008-02-23 14:04:36 -0600

    Updated merb-gen's default Rakefile, test/spec helper files, and plugin file
    Changed the default generated Rakefile to use Merb.load_dependencies instead of Merb.start.  Included a call to Merb.start in the test and spec helpers.
    Added references to the Merb::BootLoader#before_app_loads and Merb::BootLoader#after_app_loads callbacks.

--- merb-gen/app_generators/merb/templates/spec/spec_helper.rb
+++ merb-gen/app_generators/merb/templates/spec/spec_helper.rb
@@ -1,14 +1,13 @@
 $TESTING=true
 require 'rubygems'
 require 'merb-core'
-require 'merb-core/test/helpers'
-require 'merb-core/test/fake_request'
-require 'merb-core/test/request_helper'
+
 
 # TODO: Boot Merb, via the Test Rack adapter
 Merb.start :environment => (ENV['MERB_ENV'] || 'test'),
            :adapter     => 'runner',
            :merb_root  => File.join(File.dirname(__FILE__), ".." )
+
 
 Spec::Runner.configure do |config|
   config.include(Merb::Test::Helpers)
