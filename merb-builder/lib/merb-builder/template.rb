ab8318535cfbde5ff4ca97d45a89a82477eb82f9
Jonathan Younger <jonathan@daikini.com>
2008-02-23 02:03:28 -0700

    Clean up unused code

--- merb-builder/lib/merb-builder/template.rb
+++ merb-builder/lib/merb-builder/template.rb
@@ -3,14 +3,9 @@ module Merb::Template
   class Builder
     def self.compile_template(path, name, mod)
       path = File.expand_path(path)
-      config = (Merb::Plugins.config[:builder] || { }).inject({}) do |c, (k, v)|
-        c[k.to_sym] = v
-        c
-      end.merge :filename => path
-      
       method = mod.is_a?(Module) ? :module_eval : :instance_eval
       mod.send(method, %{
-        def #{name}(_builder_locals = {})
+        def #{name}
           @_engine = 'builder'
           xml = ::Builder::XmlMarkup.new :indent => 2
           self.instance_eval %{#{File.read(path)}}
