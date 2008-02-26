08ba818055fc96fbbe26fa6c7dee1bb7d8d5fe42
Yehuda Katz <wycats@gmail.com>
2008-02-26 12:16:41 -0800

    Adds a before_app_loads to merb-assets and fixes XHTML

--- merb-assets/lib/merb-assets.rb
+++ merb-assets/lib/merb-assets.rb
@@ -1,4 +1,6 @@
 require 'merb-assets/assets'
 require 'merb-assets/assets_mixin'
 
-Merb::Controller.send(:include, Merb::AssetsMixin)
+Merb::BootLoader.before_app_loads do
+  Merb::Controller.send(:include, Merb::AssetsMixin)
+end
