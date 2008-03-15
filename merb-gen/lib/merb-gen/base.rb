e4d8962df5756d72f2b3bcbed78ed1bd9844b0c1
James Herdman <james.herdman@gmail.com>
2008-03-14 20:11:21 -0400

    Clean up Merb-Gen
    Signed-off-by: Ezra Zygmuntowicz <ez@engineyard.com>

--- merb-gen/lib/merb-gen/base.rb
+++ merb-gen/lib/merb-gen/base.rb
@@ -24,7 +24,7 @@ class Merb::GeneratorBase < RubiGen::Base
       options["spec"] = true unless options["test"]
 
       # Set directories that should be optional based on command-line args
-      @choices = %w( test spec )
+      @choices = %w(test spec)
       
       # Set the assigns that should be used for path-interpolation and building templates
       @assigns = {:base_name => File.basename(@name), :test_type => options["spec"] ? "rspec" : "test_unit"}
