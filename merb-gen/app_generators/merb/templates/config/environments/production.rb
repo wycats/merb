0af191cbf8eaeb7f3997c019c519bfeea95e7363
Matt Aimonetti <mattaimonetti@gmail.com>
2008-04-22 12:17:06 -0700

    added default file logging in production mode

--- merb-gen/app_generators/merb/templates/config/environments/production.rb
+++ merb-gen/app_generators/merb/templates/config/environments/production.rb
@@ -2,4 +2,6 @@ Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
+  c[:log_level] = :error
+  c[:log_file] = Merb.log_path + "/production.log"
 }
