bb5abda2a0cebb91800dad9ae411054aee4cacdb
Fabien Franzen <info@atelierfabien.be>
2008-05-21 22:58:42 +0200

    Added merb-gen very_thin_slice generator - similar to merb_very_flat

---
+++ merb-slices/merb_generators/very_thin_slice/templates/application.rb
@@ -0,0 +1,35 @@
+module <%= module_name %>
+  
+  # This is a very thin slice - everything is stashed into this file.
+  # All Slice code is expected to be namespaced inside this module.
+  
+  class Application < Merb::Slices::Controller
+    
+    private
+    
+    # Construct a path relative to the public directory
+    def public_path_for(type, *segments)
+      File.join(::<%= module_name %>.public_dir_for(type), *segments)
+    end
+    
+    # Construct an app-level path.
+    def app_path_for(type, *segments)
+      File.join(::<%= module_name %>.app_dir_for(type), *segments)
+    end
+    
+    # Construct a slice-level path
+    def slice_path_for(type, *segments)
+      File.join(::<%= module_name %>.dir_for(type), *segments)
+    end
+    
+  end
+  
+  class Main < Application
+    
+    def index
+      render "#{<%= module_name %>.description} (v. #{<%= module_name %>.version})"
+    end
+    
+  end
+  
+end
---
+++ merb-slices/merb_generators/very_thin_slice/templates/lib/%base_name%/merbtasks.rb
@@ -0,0 +1,50 @@
+$SLICED_APP=true # we're running inside the host application context
+
+namespace :slices do
+  namespace :<%= underscored_name %> do
+  
+    desc "Install <%= module_name %>"
+    task :install => [:preflight, :setup_directories, :copy_assets, :migrate]
+    
+    desc "Test for any dependencies"
+    task :preflight do
+      # implement this to test for structural/code dependencies
+      # like certain directories or availability of other files
+    end
+    
+    desc "Setup directories"
+    task :setup_directories do
+      puts "Creating directories for host application"
+      [:application, :view, :model, :controller, :helper, :mailer, :part, :public].each do |type|
+        if File.directory?(<%= module_name %>.dir_for(type))
+          if !File.directory?(dst_path = <%= module_name %>.app_dir_for(type))
+            relative_path = dst_path.relative_path_from(Merb.root)
+            puts "- creating directory :#{type} #{File.basename(Merb.root) / relative_path}"
+            mkdir_p(dst_path)
+          end
+        end
+      end
+    end
+    
+    desc "Copy public assets to host application"
+    task :copy_assets do
+      puts "Copying assets for <%= module_name %> - do not edit these as the will be overwritten!"
+      [:image, :javascript, :stylesheet].each do |type|
+        src_path = <%= module_name %>.dir_for(type)
+        dst_path = <%= module_name %>.app_dir_for(type)
+        Dir[src_path / '**/*'].each do |file|
+          relative_path = file.relative_path_from(src_path)
+          puts "- installing :#{type} #{relative_path}"
+          mkdir_p(dst_path / File.dirname(relative_path))
+          copy_entry(file, dst_path / relative_path, false, false, true)
+        end
+      end
+    end
+    
+    desc "Migrate the database"
+    task :migrate do
+      # implement this to perform any database related setup steps
+    end
+    
+  end
+end
