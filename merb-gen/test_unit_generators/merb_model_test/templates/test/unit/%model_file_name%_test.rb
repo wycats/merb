71451e7f625e6d0d71de3c5c36dcd386029f1a6b
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-05-25 23:36:07 +0200

    removed and moved around junk for make big changes

--- merb-gen/test_unit_generators/merb_model_test/templates/test/unit/%model_file_name%_test.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'test_helper' )
-
-class <%= "#{model_class_name}Test" %> < Test::Unit::TestCase
-
-  def test_should_be_tested
-    assert false
-  end
-
-end
