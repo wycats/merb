dc3a58924c606d04ca57ac6c10ea7367fdb80ab8
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-05-26 00:09:33 +0200

    first (untested) attempt at a model generator

--- merb-gen/lib/merb-gen/model.rb
+++ merb-gen/lib/merb-gen/model.rb
@@ -4,6 +4,34 @@ module Merb
     
     class ModelGenerator < ComponentGenerator
       
+      first_argument :name
+      second_argument :attributes, :as => :hash
+      
+      template :model do
+        source('model.rbt')
+        destination('app/models/' + file_name + '.rb')
+      end
+      
+      def class_name
+        
+      end
+      
+      def file_name
+        
+      end
+      
+      def attributes?
+        self.attributes && !self.attributes.empty?
+      end
+      
+      def attributes_for_accessor
+        self.attributes.map{|a| ":#{a.first}" }.compact.uniq.join(", ")
+      end
+      
+      def source_root
+        File.join(super, 'model')
+      end
+      
     end
     
     add :model, ModelGenerator
