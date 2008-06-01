86b5fdc401d1dc3e3ea6fe35f9831dbbc9d7df2c
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-06-01 21:10:35 +0200

    fixup for model generation with tests

--- merb-gen/lib/merb-gen/model.rb
+++ merb-gen/lib/merb-gen/model.rb
@@ -6,7 +6,7 @@ module Merb::ComponentGenerators
       This is a model generator
     DESC
     
-    option :testing_framework, :default => :rspec, :desc => 'Specify which testing framework to use (spec, test_unit)'
+    option :testing_framework, :default => :spec, :desc => 'Specify which testing framework to use (spec, test_unit)'
     
     first_argument :name, :required => true
     second_argument :attributes, :as => :array
@@ -16,8 +16,22 @@ module Merb::ComponentGenerators
       destination('app/models/' + file_name + '.rb')
     end
     
+    template :spec, :testing_framework => :spec do
+      source('spec.rbt')
+      destination('spec/models/' + file_name + '_spec.rb')
+    end
+    
+    template :test_unit, :testing_framework => :test_unit do
+      source('test_unit.rbt')
+      destination('test/models/' + file_name + '_test.rb')
+    end
+    
     def class_name
       self.name.camel_case
+    end
+    
+    def test_class_name
+      self.class_name + "Test"
     end
     
     def file_name
