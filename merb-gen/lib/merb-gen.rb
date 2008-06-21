ec3ac97448cb43ec9a208b56f3f61e7096268f09
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-06-21 21:43:24 +0200

    renamed merb generator

--- merb-gen/lib/merb-gen.rb
+++ merb-gen/lib/merb-gen.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'templater'
 
-%w(generator application controller model resource_controller resource).each do |file|
+%w(generator merb controller model resource_controller resource).each do |file|
   require File.join(File.dirname(__FILE__), "merb-gen", file)
 end
