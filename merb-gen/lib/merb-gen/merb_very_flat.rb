0be732b8cdb231e4bee125fb7576e96808869f2b
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-06-21 22:07:50 +0200

    fixed very flat generator

---
+++ merb-gen/lib/merb-gen/merb_very_flat.rb
@@ -0,0 +1,36 @@
+module Merb::Generators
+  
+  class MerbVeryFlatGenerator < ApplicationGenerator
+    
+    desc <<-DESC
+      This generates a very flat merb application
+    DESC
+    
+    first_argument :name, :required => true
+    
+    template :application do
+      source('application.rbt')
+      destination("#{app_name}.rb")
+    end
+    
+    def app_name
+      self.name.snake_case
+    end
+    
+    def class_name
+      self.name.camel_case
+    end
+    
+    def destination_root
+      File.join(@destination_root)
+    end
+    
+    def source_root
+      File.join(super, 'merb_very_flat')
+    end
+    
+  end
+  
+  add :app_very_flat, MerbVeryFlatGenerator
+  
+end
