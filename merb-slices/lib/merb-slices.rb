295455d50129674bd53cf1fede2a598438f84bce
Michael S. Klishin <michael@novemberain.com>
2008-06-27 22:16:59 +0300

    Make merb-slices compatible with 0.9.3 again (at a price of ugly hack).

--- merb-slices/lib/merb-slices.rb
+++ merb-slices/lib/merb-slices.rb
@@ -3,7 +3,7 @@ if defined?(Merb::Plugins)
   # so raise unless it's so: usually happens in test
   # environment with Merb.start_environment is placed
   # below require 'merb-slices'.
-  raise "you have to start Merb environment before loading slices: make sure you have Merb.start_environment line above require 'merb-slices' in your spec helper. This usually should never arise in development/production environments." unless Merb.started?
+  raise "you have to start Merb environment before loading slices: make sure you have Merb.start_environment line above require 'merb-slices' in your spec helper. This usually should never arise in development/production environments." unless Merb.instance_variable_get("@started")
   
   require 'merb-slices/module'
   
