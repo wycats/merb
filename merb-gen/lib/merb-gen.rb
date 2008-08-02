220fc1f6f6f563339d1a1fc78ca1bd1a7ac9be4e
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-08-02 19:08:55 +0200

    updated templater dependecy version

--- merb-gen/lib/merb-gen.rb
+++ merb-gen/lib/merb-gen.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'merb-core'
 require 'sha1'
-gem 'templater', '>= 0.1.2'
+gem 'templater', '>= 0.1.4'
 require 'templater'
 
 path = File.join(File.dirname(__FILE__), "merb-gen")
