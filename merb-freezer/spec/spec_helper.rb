a532448d534684722055874373134211e0246414
Michael S. Klishin <michael@novemberain.com>
2008-08-02 04:38:11 +0300

    Removed artefacts from merb-freezer spec helper.

--- merb-freezer/spec/spec_helper.rb
+++ merb-freezer/spec/spec_helper.rb
@@ -1,7 +1,3 @@
 $TESTING=true
-<<<<<<< HEAD:merb-freezer/spec/spec_helper.rb
-$:.push File.join(File.dirname(__FILE__), '..', 'lib')
-=======
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'merb-freezer'
->>>>>>> new_merb_gen:merb-freezer/spec/spec_helper.rb
