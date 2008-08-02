6552ba286b5d1576f4b9c8bdca87f9428c87c2f9
Jonathan Stott <jonathan.stott@gmail.com>
2008-08-02 19:36:57 +0100

    Fixes for the merb-cache DataMapper code [#167]
    The current datamapper code in the gem is incorrect.
    
    This patch should fix all the issues, mostly via method renames, though
    some more significant changes to use new DM functionality.
    
    This patch was developed by myself and Caius in #merb

--- merb-cache/lib/merb-cache/cache-store/database-datamapper.rb
+++ merb-cache/lib/merb-cache/cache-store/database-datamapper.rb
@@ -2,11 +2,12 @@ module Merb::Cache::DatabaseStore::DataMapper
   # Module that provides DataMapper support for the database backend
 
   # The cache model
-  class CacheModel < DataMapper::Base
-    set_table_name Merb::Controller._cache.config[:table_name]
-    property  :ckey, :string, :length => 255, :lazy => false, :key => true
-    property  :data, :text, :lazy => false
-    property  :expire, :datetime, :default => nil
+  class CacheModel
+    include DataMapper::Resource
+    storage_names[:default] = Merb::Controller._cache.config[:table_name]
+    property  :ckey, String, :length => 255, :lazy => false, :key => true
+    property  :data, Text, :lazy => false
+    property  :expire, DateTime, :default => nil
 
     # Fetch data from the database using the specified key
     # The entry is deleted if it has expired
@@ -18,9 +19,9 @@ module Merb::Cache::DatabaseStore::DataMapper
     # data<String, NilClass>::
     #   nil is returned whether the entry expired or was not found
     def self.cache_get(key)
-      if entry = self.first(key)
+      if entry = self.get(key)
         return entry.data if entry.expire.nil? || DateTime.now < entry.expire
-        entry.destroy!
+        entry.destroy
       end
       nil
     end
@@ -38,9 +39,9 @@ module Merb::Cache::DatabaseStore::DataMapper
     #   - when set to false, force creation of the cache entry
     def self.cache_set(key, data, expire = nil, get = true)
       attributes = {:ckey => key, :data => data,
-        :expire => expire.nil? ? nil : expire.to_s_db }
+        :expire => expire }
       if get
-        entry = self.first(key)
+        entry = self.get(key)
         entry.nil? ? self.create(attributes) : entry.update_attributes(attributes)
       else
         self.create(attributes)
@@ -53,8 +54,8 @@ module Merb::Cache::DatabaseStore::DataMapper
     # ==== Parameter
     # key<Sting>:: The key identifying the cache entry
     def self.expire(key)
-      entry = self.first(key)
-      entry.destroy! unless entry.nil?
+      entry = self.get(key)
+      entry.destroy unless entry.nil?
     end
 
     # Expire the cache entries matching the given key
@@ -62,18 +63,17 @@ module Merb::Cache::DatabaseStore::DataMapper
     # ==== Parameter
     # key<Sting>:: The key matching the cache entries
     def self.expire_match(key)
-      #FIXME
-      database.execute("DELETE FROM #{self.table.name} WHERE ckey LIKE '#{key}%'")
+      self.all(:ckey.like => "#{key}%").destroy!
     end
 
     # Expire all the cache entries
     def self.expire_all
-      self.truncate!
+      self.all.destroy!
     end
 
     # Perform auto-migration in case the table is unknown in the database
     def self.check_table
-      self.auto_migrate! unless self.table.exists?
+      self.auto_upgrade!
     end
   end
 end
