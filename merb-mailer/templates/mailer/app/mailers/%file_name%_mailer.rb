b77afc93e91e725026796ea2446c2962e5089a28
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-08-04 17:11:48 +0200

    fixed mailer generator

--- merb-mailer/templates/mailer/app/mailers/%file_name%_mailer.rb
+++ merb-mailer/templates/mailer/app/mailers/%file_name%_mailer.rb
@@ -1,20 +1,11 @@
-<% counter = 0 -%>
-<% controller_modules.each_with_index do |mod, i| -%>
-<%= "  " * i %>module <%= mod %>
-<% counter = i -%>
-<% end -%>
-<% counter = counter == 0 ? 0 : (counter + 1) -%>
-<%= "  " * counter %>class <%= controller_class_name %>Mailer < Merb::MailController
+<% with_modules(modules) do -%>
+class <%= class_name %>Mailer < Merb::MailController
+
+  def notify_on_event
+    # use params[] passed to this controller to get data
+    # read more at http://wiki.merbivore.com/pages/mailers
+    render_mail
+  end
   
-<%= "  " * counter %>  def notify_on_event
-<%= "  " * counter %>    # use params[] passed to this controller to get data
-<%= "  " * counter %>    # read more at http://wiki.merbivore.com/pages/mailers
-<%= "  " * counter %>    render_mail
-<%= "  " * counter %>  end
-  
-<%= "  " * counter %>end
-<% counter = counter == 0 ? 0 : (counter - 1) -%>
-<% controller_modules.reverse.each_with_index do |mod, i| -%>
-<%= "  " * counter %>end # <%= mod %>
-<% counter = counter - 1 -%>
+end
 <% end -%>
