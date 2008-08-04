b171577705a9434fba054ead0f15277f39f31395
Jonas Nicklas <jonas.nicklas@gmail.com>
2008-08-04 17:11:48 +0200

    fixed mailer generator

--- merb-mailer/lib/generators/mailer_generator.rb
+++ merb-mailer/lib/generators/mailer_generator.rb
@@ -15,17 +15,17 @@ module Merb::Generators
     
     template :mailer do
       source('app/mailers/%file_name%_mailer.rb')
-      destination("app/mailers/#{file_name}_mailer.rb")
+      destination("app/mailers", base_path, "#{file_name}_mailer.rb")
     end
     
     template :notify_on_event do
       source('app/mailers/views/%file_name%_mailer/notify_on_event.text.erb')
-      destination("app/mailers/views/#{file_name}_mailer/notify_on_event.text.erb")
+      destination("app/mailers/views", base_path, "#{file_name}_mailer/notify_on_event.text.erb")
     end
     
     template :controller_spec, :testing_framework => :rspec do
       source('spec/mailers/%file_name%_mailer_spec.rb')
-      destination("spec/mailers/#{file_name}_mailer_spec.rb")
+      destination("spec/mailers", base_path, "#{file_name}_mailer_spec.rb")
     end
     
     def modules
@@ -41,14 +41,22 @@ module Merb::Generators
     end
     
     def test_class_name
-      controller_class_name + "Test"
+      class_name + "Test"
     end
     
     def file_name
-      controller_class_name.snake_case
+      class_name.snake_case
+    end
+    
+    def base_path
+      File.join(*snake_cased_chunks[0..-2])
     end
     
     protected
+    
+    def snake_cased_chunks
+      chunks.map { |c| c.snake_case }
+    end
     
     def chunks
       name.gsub('/', '::').split('::').map { |c| c.camel_case }
