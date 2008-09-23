fb5cde29f03069707a464e9a8873777b8e9c0126
Matt Aimonetti <mattaimonetti@gmail.com>
2008-09-23 11:08:57 -0700

    fixed commit http://github.com/wycats/merb-more/commit/c535a0376b3a5118e2545e8816d348b6cafafb5a which was causing major haml issues + moving merb haml settings to Merb::Plugins and added a notice to the logger

--- merb-haml/lib/merb-haml.rb
+++ merb-haml/lib/merb-haml.rb
@@ -3,12 +3,21 @@ if defined?(Merb::Plugins)
   require "haml"
   require "merb-haml/template"
   Merb::Plugins.add_rakefiles(File.join(File.dirname(__FILE__) / "merb-haml" / "merbtasks"))
+  
+  Merb::Plugins.config[:sass] ||= {}
 
   Merb::BootLoader.after_app_loads do
-    if File.directory?(Merb::Config[:sass][:template_location] || Merb.dir_for(:stylesheet) / "sass")
+    
+    if File.directory?(Merb::Plugins.config[:sass][:template_location] || Merb.dir_for(:stylesheet) / "sass")
       require "sass/plugin" 
-      Sass::Plugin.options = Merb::Config[:sass] if Merb::Config[:sass]
+      if Merb::Config[:sass]
+        Merb.logger.info("Please define your sass settings in Merb::Plugins.config[:sass] not Merb::Config")
+        Sass::Plugin.options = Merb::Config[:sass]
+      else
+        Sass::Plugin.options = Merb::Plugins.config[:sass]
+      end
     end
+    
   end
   
   # Hack because Haml uses symbolize_keys
