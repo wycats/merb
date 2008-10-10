3476778cbaabcc9763be2adfc37ddec7d9633a48
Wesley Beary <monki@geemus.com>
2008-10-10 15:18:51 -0700

    cleanup the exceptions_helper include

--- merb-exceptions/lib/merb-exceptions/exceptions_helper.rb
+++ merb-exceptions/lib/merb-exceptions/exceptions_helper.rb
@@ -4,19 +4,11 @@ module MerbExceptions
     # This will always render, but will also send a notification if
     # Merb::Plugins.config[:exceptions][:environments] includes the
     # current environment.
-    def self.included(mod)
+    def render_and_notify(*opts)
       if Merb::Plugins.config[:exceptions][:environments].include?(Merb.env)
-        mod.class_eval do
-          def render_and_notify(*opts)
-            self.render_then_call(render(*opts)) { notify_of_exceptions }
-          end
-        end
+        self.render_then_call(render(*opts)) { notify_of_exceptions }
       else
-        mod.class_eval do
-          def render_and_notify(*opts)
-            self.render(*opts)
-          end
-        end
+        self.render(*opts)
       end
     end
   end
