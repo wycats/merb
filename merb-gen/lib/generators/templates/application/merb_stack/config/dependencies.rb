7b7e1ec954aa45738a0b8f4fbefefa83505642c0
Matt Aimonetti <mattaimonetti@gmail.com>
2008-10-12 00:00:08 -0700

    removed dm-sweatshop dep since it's only needed in the testing env

--- merb-gen/lib/generators/templates/application/merb_stack/config/dependencies.rb
+++ merb-gen/lib/generators/templates/application/merb_stack/config/dependencies.rb
@@ -9,7 +9,6 @@ dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equiv
 dependency "dm-core", "0.9.6"         # The datamapper ORM
 dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
 dependency "dm-migrations", "0.9.6"   # Make incremental changes to your database.
-dependency "dm-sweatshop", "0.9.6"    # Generate pseudorandom models for testing or populating your development database
 dependency "dm-timestamps", "0.9.6"   # Automatically populate created_at, created_on, etc. when those properties are present.
 dependency "dm-types", "0.9.6"        # Provides additional types, including csv, json, yaml.
 dependency "dm-validations", "0.9.6"  # Validation framework
