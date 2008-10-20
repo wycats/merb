b1b3fbff98cebf1f11d88d97848027d2ee6699a4
Daniel Neighman <has.sox@gmail.com>
2008-10-21 01:38:30 +1100

    Updates The exceptions controller so it doesn't inherit from application. Updates the merb-auth package so it does not rely on knowing in advance which Exceptions controller it is.  i.e. inheriting from Applicaion or Merb::Controller.  Sorts http://merb.lighthouseapp.com/projects/7435/tickets/289

--- merb-auth/merb-auth-slice-password/app/controllers/exceptions.rb
+++ merb-auth/merb-auth-slice-password/app/controllers/exceptions.rb
@@ -1,18 +1,7 @@
-class Application < Merb::Controller; end
-
-class Exceptions < Application
-  include Merb::Slices::Support # Required to provide slice_url
-  
-  # # This stuff allows us to provide a default view
-  the_view_path = File.expand_path(File.dirname(__FILE__) / ".." / "views")
-  self._template_roots ||= []
-  self._template_roots << [the_view_path, :_template_location]
-  self._template_roots << [Merb.dir_for(:view), :_template_location]
-    
+# the mixin to provide the exceptions controller action for Unauthenticated
+module MerbAuthSlicePassword::ExceptionsMixin
   def unauthenticated
     provides :xml, :js, :json, :yaml
-    
-    session.abandon!
 
     case content_type
     when :html
@@ -21,6 +10,24 @@ class Exceptions < Application
       basic_authentication.request!
       ""
     end
-  end
-  
+  end # unauthenticated
 end
+
+Merb::Authentication.customize_default do
+  
+  Exceptions.class_eval do
+    include Merb::Slices::Support # Required to provide slice_url
+  
+    # # This stuff allows us to provide a default view
+    the_view_path = File.expand_path(File.dirname(__FILE__) / ".." / "views")
+    self._template_roots ||= []
+    self._template_roots << [the_view_path, :_template_location]
+    self._template_roots << [Merb.dir_for(:view), :_template_location]
+    
+    include MerbAuthSlicePassword::ExceptionsMixin
+    
+    show_action :unauthenticated
+
+  end# Exceptions.class_eval
+  
+end # Customize default
