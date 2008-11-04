3590a5047d5fce0ddd151d230be8bd8daf0208fc
Carl Lerche <carllerche@mac.com>
2008-11-03 19:34:03 -0800

    Removing the hpricot gem dependency

--- merb-core/lib/merb-core/test.rb
+++ merb-core/lib/merb-core/test.rb
@@ -1,9 +1,3 @@
-# begin
-#   require "hpricot"
-#   require 'merb-core/test/test_ext/hpricot'
-# rescue
-# end
-
 require 'merb-core/test/test_ext/object'
 require 'merb-core/test/test_ext/string'
 
