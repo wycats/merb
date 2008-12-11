573a9a32166d404546e4140555691fa588787790
Matt Aimonetti <mattaimonetti@gmail.com>
2008-12-11 00:19:54 -0800

    cleaned up the default generated controller

--- merb-gen/lib/generators/templates/component/controller/app/controllers/%file_name%.rb
+++ merb-gen/lib/generators/templates/component/controller/app/controllers/%file_name%.rb
@@ -1,8 +1,6 @@
 <% with_modules(modules) do -%>
 class <%= class_name %> < Application
 
-  # ...and remember, everything returned from an action
-  # goes to the client...
   def index
     render
   end
