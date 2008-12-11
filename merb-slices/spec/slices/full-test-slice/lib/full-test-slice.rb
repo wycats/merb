da1fb2d25175ab1eb8a52ecfd23a9797581e69d0
Matt Aimonetti <mattaimonetti@gmail.com>
2008-12-10 17:14:08 -0800

    modified the specs to not use a private API method, still 2 failing specs tho

--- merb-slices/spec/slices/full-test-slice/lib/full-test-slice.rb
+++ merb-slices/spec/slices/full-test-slice/lib/full-test-slice.rb
@@ -2,7 +2,7 @@ if defined?(Merb::Plugins)
 
   $:.unshift File.dirname(__FILE__)
 
-  load_dependency 'merb-slices'
+  dependency 'merb-slices', :immediate => true
   Merb::Plugins.add_rakefiles "full-test-slice/merbtasks", "full-test-slice/slicetasks", "full-test-slice/spectasks"
 
   # Register the Slice for the current host application
--- merb-slices/spec/slices/very-thin-test-slice/lib/very-thin-test-slice.rb
+++ merb-slices/spec/slices/very-thin-test-slice/lib/very-thin-test-slice.rb
@@ -2,7 +2,7 @@ if defined?(Merb::Plugins)
 
   $:.unshift File.dirname(__FILE__)
 
-  load_dependency 'merb-slices'
+  dependency 'merb-slices', :immediate => true
   Merb::Plugins.add_rakefiles "very-thin-test-slice/merbtasks", "very-thin-test-slice/slicetasks"
 
   # Register the Slice for the current host application
