66c393b31bb93e5a26c2b0c08fcc9039c61d9499
Yehuda Katz <wycats@gmail.com>
2008-12-14 11:07:09 -0800

    Remove autoload to work around bug in Ruby's autoload

--- merb-core/lib/merb-core/autoload.rb
+++ merb-core/lib/merb-core/autoload.rb
@@ -1,31 +1,30 @@
+require 'merb-core/core_ext'
+require "merb-core/controller/exceptions"
+require "merb-core/controller/mixins/responder"
+require "merb-core/controller/mixins/render"
+require "merb-core/controller/mixins/authentication"
+require "merb-core/controller/mixins/conditional_get"
+require "merb-core/controller/mixins/controller"
+require "merb-core/controller/abstract_controller"
+require "merb-core/controller/template"
+require "merb-core/controller/merb_controller"
+require "merb-core/bootloader"
+require "merb-core/config"
+require "merb-core/constants"
+require "merb-core/dispatch/dispatcher"
+require "merb-core/plugins"
+require "merb-core/rack"
+require "merb-core/dispatch/request"
+require "merb-core/dispatch/request_parsers.rb"
+require "merb-core/dispatch/router"
+require "merb-core/dispatch/worker"
+
 module Merb
-  autoload :AbstractController,       "merb-core/controller/abstract_controller"
-  autoload :BootLoader,               "merb-core/bootloader"
-  autoload :Config,                   "merb-core/config"
-  autoload :Const,                    "merb-core/constants"
-  autoload :ConditionalGetMixin,      "merb-core/controller/mixins/conditional_get"
-  autoload :ControllerMixin,          "merb-core/controller/mixins/controller"
-  autoload :ControllerExceptions,     "merb-core/controller/exceptions"
-  autoload :Dispatcher,               "merb-core/dispatch/dispatcher"
-  autoload :AuthenticationMixin,      "merb-core/controller/mixins/authentication"
-  autoload :BasicAuthenticationMixin, "merb-core/controller/mixins/authentication/basic"
-  autoload :ErubisCaptureMixin,       "merb-core/controller/mixins/erubis_capture"
-  autoload :Plugins,                  "merb-core/plugins"
-  autoload :Rack,                     "merb-core/rack"
-  autoload :RenderMixin,              "merb-core/controller/mixins/render"
-  autoload :Request,                  "merb-core/dispatch/request"
-  autoload :Parse,                    "merb-core/dispatch/request_parsers.rb"
-  autoload :ResponderMixin,           "merb-core/controller/mixins/responder"
-  autoload :Router,                   "merb-core/dispatch/router"
-  autoload :Test,                     "merb-core/test"
-  autoload :Worker,                   "merb-core/dispatch/worker"
+  autoload :Test, "merb-core/test"
 end
 
 # Require this rather than autoloading it so we can be sure the default template
 # gets registered
-require 'merb-core/core_ext'
-require "merb-core/controller/template"
-require "merb-core/controller/merb_controller"
 
 module Merb
   module InlineTemplates; end
