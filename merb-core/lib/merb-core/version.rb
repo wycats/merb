5947c3943b456b41e2fdec6f806f301f61c9fdb1
Yehuda Katz <wycats@gmail.com>
2008-12-16 14:45:06 -0800

    Prepping 1.0.6.1 release

--- merb-core/lib/merb-core/version.rb
+++ merb-core/lib/merb-core/version.rb
@@ -1,4 +1,4 @@
 module Merb
-  VERSION = '1.0.6.0.1' unless defined?(Merb::VERSION)
+  VERSION = '1.0.6.1' unless defined?(Merb::VERSION)
   DM_VERSION = '0.9.8' unless defined?(Merb::DM_VERSION)
 end
