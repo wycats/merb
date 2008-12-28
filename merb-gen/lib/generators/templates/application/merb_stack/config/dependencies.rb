7243f0a6fb16f03c5b5e255f095c91ced9654e06
Yehuda Katz <wycats@gmail.com>
2008-12-28 10:59:27 -0800

    Update generator to correctly ignore all gems/gems and put in DM and do_sqlite3 into generated deps.rb

--- merb-gen/lib/generators/templates/application/merb_stack/config/dependencies.rb
+++ merb-gen/lib/generators/templates/application/merb_stack/config/dependencies.rb
@@ -21,3 +21,6 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
+
+dependency "merb_datamapper", merb_gems_version
+dependency "do_sqlite3" # If using another database, replace this
