b5a3c222a2f0887a2f97048a0b0a8cffca17f0cc
Matt Aimonetti <mattaimonetti@gmail.com>
2009-02-16 11:14:26 -0800

    bumped version to 1.1

--- merb-core/lib/merb-core/version.rb
+++ merb-core/lib/merb-core/version.rb
@@ -1,5 +1,5 @@
 module Merb
-  VERSION    = '1.0.9'  unless defined?(Merb::VERSION)
+  VERSION    = '1.1'  unless defined?(Merb::VERSION)
   DM_VERSION = '0.9.10' unless defined?(Merb::DM_VERSION)
   DO_VERSION = '0.9.11' unless defined?(Merb::DO_VERSION)
 end
