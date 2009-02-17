4c16ccd03df298090caee3085ada91acb53fe663
takiuchi <genki@s21g.com>
2009-02-17 21:18:45 +0900

    Fixed a bug

--- merb-core/lib/merb-core/test/run_spec.rb
+++ merb-core/lib/merb-core/test/run_spec.rb
@@ -25,7 +25,7 @@ def run_spec(spec, base_dir, run_opts = "-fs")
   $VERBOSE = nil
   err, out = StringIO.new, StringIO.new
   def out.tty?() true end
-  options = Spec::Runner::OptionParser.parse(%W(#{spec} --color #{run_opts}), err, out)
+  options = Spec::Runner::OptionParser.parse(%W(#{spec} --color).concat(%W(#{run_opts})), err, out)
   options.filename_pattern = File.expand_path(spec)
   failure = ! Spec::Runner::CommandLine.run(options)
   File.open(base_dir / "results" / "#{File.basename(spec)}_out", "w") do |file|
