afa4299662af2e184e64b942e3a60b93d16c2c40
Matt Aimonetti <mattaimonetti@gmail.com>
2009-03-18 13:22:16 -0700

    improved suport for unnamed modules (ruby1.9 patch provided by Maiha)

--- merb-core/lib/merb-core/controller/abstract_controller.rb
+++ merb-core/lib/merb-core/controller/abstract_controller.rb
@@ -241,6 +241,9 @@ module Merb
     def self.inherited(klass)
       _abstract_subclasses << klass.to_s
       helper_module_name = klass.to_s =~ /^(#|Merb::)/ ? "#{klass}Helper" : "Merb::#{klass}Helper"
+      # support for unnamed module like "#<Class:0xa2e5e50>::TestController"
+      helper_module_name.gsub!(/(::)|[:#<>]/, "\\1")
+
       Object.make_module helper_module_name
       klass.class_eval <<-HERE
         include Object.full_const_get("#{helper_module_name}") rescue nil
