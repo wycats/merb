7dc1cc7a93f687fc4dd3f1e6b7feefcc31fe7594
Matt Aimonetti <mattaimonetti@gmail.com>
2009-03-23 19:51:45 -0700

    started the wrapper

---
+++ merb-actionorm/lib/merb-actionorm/action_orm/core.rb
@@ -0,0 +1,76 @@
+#--
+# Copyright (c) 2004-2008 David Heinemeier Hansson
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+module ActionORM
+  class ActionORMError < StandardError; end
+  class DriverNotFoundException < ActionORMError; end
+  class AbstractDriverMethod < ActionORMError; end
+  class UnknownORM < ActionORMError; end
+
+  module Core
+    module ClassMethods
+      def supports?(obj)
+        find_key(obj)
+      end
+
+      def for(obj)
+        driver = driver_registry[find_key(obj)]
+        case driver
+        when NilClass
+          obj
+        else
+          driver.new(obj)
+        end
+      end
+
+      def use(options)
+        case options[:orm].to_s
+        when /test[_\W\s]?orm/i
+          options = {:for => TestORMModel, :driver => Drivers::TestORMDriver}.merge(options)
+        when /sequel/i
+          options = {:for => Sequel::Model, :driver => Drivers::SequelDriver}.merge(options)
+        end
+        ActionORM.register(options[:for], options[:driver])
+      end
+      
+      protected
+        def driver_registry
+          @_driver_registry ||= {}
+        end
+        
+        def driver_key_cache
+          @_driver_key_cache ||= {}
+        end
+      
+        def register(obj_class, obj_driver_class)
+          driver_registry[obj_class] = obj_driver_class
+        end
+        
+        def find_key(obj)
+          unless driver_key_cache.key?(obj.class)
+            driver_key_cache[obj.class] = driver_registry.keys.find {|k, v| obj.is_a? k }
+          end
+          driver_key_cache[obj.class]
+        end
+    end
+  end
+end
---
+++ merb-actionorm/spec/spec_helper.rb
@@ -0,0 +1,31 @@
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+require 'merb-actionorm'
+action_orm_path = File.join(File.dirname(__FILE__), '..', 'lib', 'merb-actionorm', 'action_orm')
+require File.join(action_orm_path, 'test_orm_model')
+require File.join(action_orm_path, 'drivers','test_orm_driver')
+
+begin
+  gem 'sequel'
+rescue LoadError => load_error
+  puts "To run the specs properly, you need to have sequel installed #{load_error}"
+else
+  require 'sequel'
+end
+
+begin
+  gem 'dm-core'
+  gem 'dm-validations'
+rescue LoadError => load_error
+  puts "To run the specs properly, you need to have DataMapper installed #{load_error}"
+else
+  require 'dm-core'
+  require 'dm-validations'
+end
+
+begin
+  gem 'activerecord'
+rescue LoadError => load_error
+  puts "To run the specs properly, you need to have active_record installed #{load_error}"
+else
+  require 'activerecord'
+end
