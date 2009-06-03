26a2c607ec8200182bec2e63ed83f51fbf2b9abe
snusnu <gamsnjaga@gmail.com>
2009-06-03 02:34:28 +0200

    [merb_datamapper] removed dm-timestamps override
    * this should be fixed inside dm-core

--- merb_datamapper/lib/merb_datamapper.rb
+++ merb_datamapper/lib/merb_datamapper.rb
@@ -88,21 +88,4 @@ if defined?(Merb::Plugins)
   Merb.add_generators generators / 'data_mapper_resource_controller'
   Merb.add_generators generators / 'data_mapper_migration'
   
-  # Override bug in DM::Timestamps
-  Merb::BootLoader.after_app_loads do
-    module DataMapper
-      module Timestamp
-        private
-        
-        def set_timestamps
-          return unless dirty? || ActionORM.for(self).new_record?
-          TIMESTAMP_PROPERTIES.each do |name,(_type,proc)|
-            if model.properties.has_property?(name)
-              model.properties[name].set(self, proc.call(self, model.properties[name])) unless attribute_dirty?(name)
-            end
-          end
-        end
-      end
-    end
-  end
 end
