161a6261089aa1a2eeaf36a5fa69bd95dac90072
snusnu <gamsnjaga@gmail.com>
2009-10-02 14:57:38 +0200

    Bumped version in preparation for release

--- merb-core/lib/merb-core/version.rb
+++ merb-core/lib/merb-core/version.rb
@@ -1,5 +1,5 @@
 module Merb
-  VERSION    = '1.0.12' unless defined?(Merb::VERSION)
-  DM_VERSION = '0.10.0' unless defined?(Merb::DM_VERSION)
-  DO_VERSION = '0.10.0' unless defined?(Merb::DO_VERSION)
+  VERSION    = '1.0.13' unless defined?(Merb::VERSION)
+  DM_VERSION = '0.10' unless defined?(Merb::DM_VERSION)
+  DO_VERSION = '0.10' unless defined?(Merb::DO_VERSION)
 end
