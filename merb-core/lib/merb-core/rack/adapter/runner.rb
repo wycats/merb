2d67468d02e6091edcb24b222fec8a9fd4e49f7e
Jacques Crocker <merbjedi@gmail.com>
2009-10-16 12:32:10 -0700

    [merb-core] Better error handling for Runner Adapter
    Added a error handler for when the user tries to
    run a script (using the Merb Runner Adapter) that
    looks like a file but doesn't exist.
    
    [#1231 state:resolved]

--- merb-core/lib/merb-core/rack/adapter/runner.rb
+++ merb-core/lib/merb-core/rack/adapter/runner.rb
@@ -16,11 +16,24 @@ module Merb
       # :api: plugin
       def self.start(opts={})
         Merb::Server.change_privilege
+
         if opts[:runner_code]
+          runner_code = opts[:runner_code]
           if File.exists?(opts[:runner_code])
-            eval(File.read(opts[:runner_code]), TOPLEVEL_BINDING, __FILE__, __LINE__)
-          else
-            eval(opts[:runner_code], TOPLEVEL_BINDING, __FILE__, __LINE__)
+            runner_code = File.read(runner_code)
+            runner_script = true
+          end
+
+          begin
+            eval(runner_code, TOPLEVEL_BINDING, __FILE__, __LINE__)
+          rescue Exception => e
+            # check to see if user gave us a string that links like they tried to run a script file
+            if !runner_script && (runner_code.include?(File::SEPARATOR) || runner_code.include?(".rb"))
+              Merb.logger.error!("Merb Runner Adapter - tried to execute script file")
+              Merb.logger.error!("Not Found: #{runner_code}")
+            else
+              raise e
+            end
           end
           exit
         end  
